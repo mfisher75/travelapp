d8b26ede85c67e5c37d95a3678f82edd7346305c
Moshe Fisher <mf840u@intl.att.com>
2018-07-28 17:32:32 +0300

    jbkjbkj

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,3 +12,17 @@ export const environment = {
   DB_NAME: 'travelapp',
 };
 
+
+// {
+//   "apikey": "KUi8wsUKvWpUpkxZ1rFCt4y6Z0O1TSOIha1YVtWkjL2E",
+//   "host": "d9a5c4d0-8b49-4dd6-b048-875eec92a612-bluemix.cloudant.com",
+//   "iam_apikey_description": "Auto generated apikey during resource-key operation for Instance - crn:v1:bluemix:public:cloudantnosqldb:eu-gb:a/376f3c458c7847a1a94b0a24c14517a9:2e2e8d77-70ad-47be-b4f6-d6638205c4f2::",
+//   "iam_apikey_name": "auto-generated-apikey-d82d2f80-5872-4636-96bc-ef1ca273720b",
+//   "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
+//   "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/376f3c458c7847a1a94b0a24c14517a9::serviceid:ServiceId-e7e2524c-260e-44d5-9793-b3cac93730d7",
+//   "password": "96dda27a76bee4a304db174311ad8dd1467bb42bd6062977965f5bb98b8b4458",
+//   "port": 443,
+//   "url": "https://d9a5c4d0-8b49-4dd6-b048-875eec92a612-bluemix:96dda27a76bee4a304db174311ad8dd1467bb42bd6062977965f5bb98b8b4458@d9a5c4d0-8b49-4dd6-b048-875eec92a612-bluemix.cloudant.com",
+//   "username": "d9a5c4d0-8b49-4dd6-b048-875eec92a612-bluemix"
+// }
+
